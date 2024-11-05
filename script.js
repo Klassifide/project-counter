a2c193c890d7363d1e3ded51dfb6677d55413096
Navarone <klassifide@outlook.co.nz>
2024-11-05 13:29:02 +1300

    Step4/5

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-script.js
+const counterButton = document.getElementById('incrementButton');
+
+function add1() {
+    alert("Hey Ho Kleiner Stern");
+}
+
+
+
+counterButton.addEventListener('click', add1);
