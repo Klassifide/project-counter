5fb526bc03f04122bb51d099f4d7b200bd77ceff
Navarone <klassifide@outlook.co.nz>
2024-11-05 14:41:24 +1300

    Step 8

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+//fundamental code for the starting operations of the counter
 const counterButton = document.getElementById('incrementButton');
 const label = document.getElementById('displayCounter');
 let count = 0;
 
+// function to add 1 to the counter
 function add1() {
    count++;
    const word = count == 1 ? 'time' : 'times';
    label.innerText = `Clicked ${count} ${word}`;
 }
-
+//event listener for the function upon click
 counterButton.addEventListener('click', add1);
